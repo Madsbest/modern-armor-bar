b1b2873683faa1a01f12fba84b4bf8916634476c
Madsbest <madswesterhof365@outlook.com>
2026-05-10 20:28:43 +0200

    Create ModernArmorBarClient.java

---
+++ src/client/java/com/mads/armorbar/client/ModernArmorBarClient.java
@@ -0,0 +1,8 @@
+package com.mads.armorbar.client;
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModernArmorBarClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
